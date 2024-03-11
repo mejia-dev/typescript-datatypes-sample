81a428dbef41e6723a13a8640d792028a839fedb
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-11 15:34:29 -0700

    Generic function practice

--- functions.ts
+++ functions.ts
@@ -17,3 +17,16 @@ function add2(num1: number, num2: number, ...num3: number[]): number {
 
 let numbers = [1, 2, 3, 4, 5];
 console.log(add2(2, 3, ...numbers));
+
+
+
+// GENERIC FUNCTIONS
+
+
+// "Type" is a placeholder value and can be replace with anything.
+function getItems<Type>(items: Type[]): Type[] {
+  return new Array<Type>().concat(items);
+}
+
+let concatNumResult = getItems<number>([1,2,3,4]);
+let concatStringResult = getItems<string>(["1","2","testing!","oh yeah"]);
