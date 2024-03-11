f72383c6093cd750512f75f50df81d9a73f10742
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-11 16:03:36 -0700

    Static, getter, setter, private, and protected methods practice

--- class.ts
+++ class.ts
@@ -5,10 +5,49 @@ class Employee {
   address!:string;
 }
 
+class EmployeeV2 {
+  // hash sets something to private instead of putting private keyword before it. 
+  #id: number;
+
+  // Getter method
+  get empId(): number {
+    return this.#id;
+  }
+
+  // setter method
+  set empId(id : number) {
+    this.#id = id;
+  }
+
+  protected name: string;
+
+  address:string;
+
+  // static method with type
+  static getEmployeeCount() : number { 
+    return 50;
+  }
+
+  constructor(id: number, name: string, address: string) {
+    this.address = address;
+    this.name = name;
+    this.#id = id;
+  }
+}
+
 let john = new Employee();
+let john2 = new EmployeeV2(2, "John", "Highway 2");
+
+class Manager extends EmployeeV2 {
+  constructor(id: number, name: string, address: string) {
+    super(id, name, address);
+  }
+}
 
 john.id = 1;
 john.name = "John";
 john.address = "Highway 71";
 
 console.log(john);
+console.log(john2);
+console.log(EmployeeV2.getEmployeeCount);
