a158c37cfbb49d2d5f18b218389bea90d7715d57
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-11 16:25:41 -0700

    Interface practice

---
+++ interface.js
@@ -0,0 +1,3 @@
+"use strict";
+let user = { name: "John", id: 1, email: "" };
+let employee = { name: "John", id: 1, email: "", salary: 1000 };
---
+++ interface.ts
@@ -0,0 +1,14 @@
+interface User {
+  name: string;
+  age?: number;
+  id: number;
+  email: string;
+}
+
+let user: User = { name: "John", id: 1, email: "" };
+
+interface Employees extends User {
+  salary: number;
+}
+
+let employee: Employees = { name: "John", id: 1, email: "", salary: 1000 };
